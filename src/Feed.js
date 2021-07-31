8f3fd8087b92cef512e6bed1f4fad371e0c6df9d
mostafa-sadeghi <msai.sadeghi@gmail.com>
2021-07-31 00:52:12 -0700

    third commit

--- src/Feed.js
+++ src/Feed.js
@@ -1,17 +1,33 @@
 import React from "react";
 import "./Feed.css";
 import CreateIcon from "@material-ui/icons/Create";
+import ImageIcon from "@material-ui/icons/Image";
+import SubscriptionsIcon from "@material-ui/icons/Subscriptions";
+import EventNoteIcon from "@material-ui/icons/EventNote";
+import CalendarViewDayIcon from "@material-ui/icons/CalendarViewDay";
+import InputOption from "./InputOption";
 
 const Feed = () => {
   return (
     <div className="feed">
       <div className="feed__inputContainer">
-        <div className="feed__input"></div>
-        <CreateIcon />
-        <form action="">
-          <input type="text" />
-          <button type="submit">Send</button>
-        </form>
+        <div className="feed__input">
+          <CreateIcon />
+          <form action="">
+            <input type="text" />
+            <button type="submit">Send</button>
+          </form>
+        </div>
+        <div className="feed_inputOptions">
+          <InputOption Icon={ImageIcon} title="photo" color="#70B5F9" />
+          <InputOption Icon={SubscriptionsIcon} title="photo" color="#E7A33E" />
+          <InputOption Icon={EventNoteIcon} title="event" color="#C0CBCD" />
+          <InputOption
+            Icon={CalendarViewDayIcon}
+            title="event"
+            color="#7Fc15E"
+          />
+        </div>
       </div>
     </div>
   );
